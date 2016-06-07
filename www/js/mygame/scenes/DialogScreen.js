ca669853b655f90799344a42589da56ad5f2e17a
raphaelstary <raphael.stary@gmail.com>
2016-06-07 18:07:30 +0200

    reading signs - should be ready for now

--- www/js/mygame/scenes/DialogScreen.js
+++ www/js/mygame/scenes/DialogScreen.js
@@ -1,44 +1,69 @@
-G.DialogScreen = (function (Event, Key) {
+G.DialogScreen = (function (Event, Key, Width, Height) {
     "use strict";
 
-    function DialogScreen(services, text) {
+    function DialogScreen(services, textPragraphs) {
         this.events = services.events;
         this.timer = services.timer;
-        this.text = text;
+        this.textPragraphs = textPragraphs.slice();
     }
 
     DialogScreen.prototype.postConstruct = function () {
         this.itIsOver = false;
+        var typing = true;
+        var skip = false;
         var self = this;
         this.keyListener = this.events.subscribe(Event.KEY_BOARD, function (keyBoard) {
             if (self.itIsOver)
                 return;
 
             if (keyBoard[Key.ENTER] || keyBoard[Key.SPACE]) {
-                self.itIsOver = true;
-                self.nextScene();
+                if (typing) {
+                    // skip typing
+                    skip = true;
+                } else {
+
+                    if (self.textPragraphs.length > 0) {
+                        typing = true;
+                        skip = false;
+                        self.dialogTxt.setText('');
+                        startNextCharacterIteration(self.textPragraphs.shift(), 0);
+                    } else {
+                        self.itIsOver = true;
+                        self.nextScene();
+                    }
+                }
             }
         });
 
-        function writeNextCharacter(index) {
-            self.dialogTxt.setText(self.dialogTxt.data.msg + self.text[index]);
+        function writeNextCharacter(text, index) {
+            if (skip) {
+                self.dialogTxt.setText(text);
+                typing = false;
+                return;
+            }
+            self.dialogTxt.setText(self.dialogTxt.data.msg + text[index]);
 
-            if (index < self.text.length - 1)
-                startNextCharacterIteration(index + 1);
+            if (index < text.length - 1) {
+                startNextCharacterIteration(text, index + 1);
+            } else {
+                typing = false;
+            }
         }
 
-        function startNextCharacterIteration(index) {
-            if (self.text[index] == ' ') {
-                writeNextCharacter(index);
+        function startNextCharacterIteration(text, index) {
+            if (text[index] == ' ') {
+                writeNextCharacter(text, index);
             } else {
                 self.timer.doLater(function () {
-                    writeNextCharacter(index);
+                    writeNextCharacter(text, index);
                 }, 2);
             }
         }
 
         self.dialogTxt.setText('');
-        startNextCharacterIteration(0);
+        self.dialogTxt.setMaxLineLength(Width.get(10, 9));
+        self.dialogTxt.setLineHeight(Height.get(10));
+        startNextCharacterIteration(this.textPragraphs.shift(), 0);
     };
 
     DialogScreen.prototype.preDestroy = function () {
@@ -46,4 +71,4 @@ G.DialogScreen = (function (Event, Key) {
     };
 
     return DialogScreen;
-})(H5.Event, H5.Key);
+})(H5.Event, H5.Key, H5.Width, H5.Height);
