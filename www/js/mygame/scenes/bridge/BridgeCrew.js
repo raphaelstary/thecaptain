00584f7fc556f750125d43ebda6018f3e90a5004
raphaelstary <raphael.stary@gmail.com>
2016-08-03 19:45:06 +0200

    pointer inputs for crew selection scene

--- www/js/mygame/scenes/bridge/BridgeCrew.js
+++ www/js/mygame/scenes/bridge/BridgeCrew.js
@@ -119,5 +119,68 @@ G.BridgeCrew = (function (Event, Key, ScreenShaker) {
         this.shaker.startSmallShake();
     };
 
+    BridgeCrew.prototype.__hideAllSelections = function () {
+        this.selectionEngineering.show = false;
+        this.selectionTactics.show = false;
+        this.selectionWeapons.show = false;
+        this.selectionNavigation.show = false;
+        this.selectionScience.show = false;
+        this.selectionCommunication.show = false;
+    };
+
+    BridgeCrew.prototype.engineeringUp = function () {
+        this.__hideAllSelections();
+        this.selectionEngineering.show = true;
+        this.showOrders(this.engineering);
+    };
+
+    BridgeCrew.prototype.engineeringDown = function () {
+    };
+
+    BridgeCrew.prototype.tacticsUp = function () {
+        this.__hideAllSelections();
+        this.selectionTactics.show = true;
+        this.showOrders(this.tactics);
+    };
+
+    BridgeCrew.prototype.tacticsDown = function () {
+    };
+
+    BridgeCrew.prototype.navigationUp = function () {
+        this.__hideAllSelections();
+        this.selectionNavigation.show = true;
+        this.showOrders(this.navigation);
+    };
+
+    BridgeCrew.prototype.navigationDown = function () {
+    };
+
+    BridgeCrew.prototype.weaponsUp = function () {
+        this.__hideAllSelections();
+        this.selectionWeapons.show = true;
+        this.showOrders(this.weapons);
+    };
+
+    BridgeCrew.prototype.weaponsDown = function () {
+    };
+
+    BridgeCrew.prototype.scienceUp = function () {
+        this.__hideAllSelections();
+        this.selectionScience.show = true;
+        this.showOrders(this.science);
+    };
+
+    BridgeCrew.prototype.scienceDown = function () {
+    };
+
+    BridgeCrew.prototype.communicationUp = function () {
+        this.__hideAllSelections();
+        this.selectionCommunication.show = true;
+        this.showOrders(this.communication);
+    };
+
+    BridgeCrew.prototype.communicationDown = function () {
+    };
+
     return BridgeCrew;
 })(H5.Event, H5.Key, H5.ScreenShaker);
